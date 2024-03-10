d97b72835eee276835ee7e5e413a310ba2c76925
Michael Ly <michaelly60@yahoo.com>
2024-03-09 18:38:43 -0600

    Started a CRUD app that stores games into sql

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+db = SQLAlchemy(app)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Game {self.title}>'
+
+@app.route('/')
+def index():
+    games = Game.query.all()
+    return render_template('index.html', games=games)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_game():
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        year = request.form['year']
+        new_game = Game(title=title, genre=genre, year=year)
+        db.session.add(new_game)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_game(id):
+    game = Game.query.get_or_404(id)
+    if request.method == 'POST':
+        game.title = request.form['title']
+        game.genre = request.form['genre']
+        game.year = request.form['year']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', game=game)
+
+@app.route('/delete/<int:id>')
+def delete_game(id):
+    game = Game.query.get_or_404(id)
+    db.session.delete(game)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
